0a6f778873c8be19aaf9534d34b425bfcc495241
Danila, Stavrov <DStavrov@alfabank.ru>
2023-12-15 02:34:32 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ PATH_TO_INDEX="$(pwd)/dist/index.html"
 
 # save backup of autostart
 date=$(date '+%Y-%m-%d')
-cp ~/.config/lxsession/LXDE/autostart  "~/.config/lxsession/LXDE/autostart-${date}.backup"
+sudo cp ~/.config/lxsession/LXDE/autostart  "$(pwd)/autostart-${date}.backup"
 
 # create new autostart
-cp ./autostart  ~/.config/lxsession/LXDE/autostart
-sed -i -e 's/\<PATH\>/${PATH_TO_INDEX}/g' ~/.config/lxsession/LXDE/autostart
+sudo cp ./autostart  ~/.config/lxsession/LXDE/autostart
+sed -i -e "s/\<PATH\>/${PATH_TO_INDEX}/g" ~/.config/lxsession/LXDE/autostart
